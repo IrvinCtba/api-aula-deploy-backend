7664b8b31eb46ee6e2d76d15bc57b2b67484692d
Rubens Almeida <rubens_ctba@me.com>
2023-01-13 10:29:56 -0300

    atualizacao 1.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
-
+const knex = require("./conexao.js");
 const app = express();
 
 app.use(express.json());
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const response = await knex("deployaula").select("*");
+    return res.json(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.message);
+  }
+});
 
 app.get("/", async (req, res) => {
   return res.json("API ok");
